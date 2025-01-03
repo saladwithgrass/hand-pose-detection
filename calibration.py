0436f800094440657cc0ca052a930ebeedeb669c
saladwithgrass <egod09080908@yandex.ru>
2025-01-03 16:04:22 +0300

    added calibnration, but need to make it more ergonomic

--- calibration.py
+++ calibration.py
@@ -1,10 +1,13 @@
-from tqdm import tqdm
 import cv2
 import numpy as np
 import argparse
 import pickle
 
 def generate_3d_points_for_marker(size_m:float):
+    """
+    Generates a 4x3 matrix of marker coordinates on a flat plane.
+    Center of the marker is (0, 0, 0).
+    """
     half_size = size_m / 2
     points = [
         [-half_size, half_size, 0], # lu
@@ -12,9 +15,13 @@ def generate_3d_points_for_marker(size_m:float):
         [half_size, -half_size, 0], #rb
         [-half_size, -half_size, 0.] # lb
         ]
-    return np.array(points)
+    return np.array(points, dtype=np.float32)
     
 def detect_aruco_in_image(image, parameters, dictionary_name):
+    """
+    Detects aruco marker in an image with given parameters.
+    Returns only corners and ids.
+    """
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     dictionary = cv2.aruco.getPredefinedDictionary(dictionary_name)
     corners, ids, _ = cv2.aruco.detectMarkers(
@@ -22,9 +29,10 @@ def detect_aruco_in_image(image, parameters, dictionary_name):
         dictionary=dictionary,
         parameters=parameters
         )
-    return np.array(corners), ids
+    return np.array(corners, dtype=np.float32), ids
 
 def main():
+    # add CLI arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('camera_id', help='camera id in /dev/video*')
     parser.add_argument('-ms', '--marker-size', help='Length of one side of the detection of a square marker in meters.', default=0.1)
@@ -33,34 +41,57 @@ def main():
     buffer_size = args.buffer_size
     source_id = int(args.camera_id)
 
+    # open camera for reading 
     cap = cv2.VideoCapture(source_id, cv2.CAP_V4L2)
+    # set buffer size
     cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
 
+    # specify aruco dictionary    
     aruco_dictionary = cv2.aruco.DICT_4X4_50
+    # create detector parameters
     aruco_detector_parameters = cv2.aruco.DetectorParameters()
     aruco_detector_parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
 
+    # create a generic matrix for marker coordinates
     aruco_points = generate_3d_points_for_marker(0.1)
-
+    
+    # create empty arrays to store
     object_points = []
     image_points = []
     while True:
+        # capture frame
         ret, frame = cap.read()
+        
+        # check success
         if not ret:
             break
-        detected_corners, ids = detect_aruco_in_image(frame, aruco_detector_parameters, aruco_dictionary)
-        if detected_corners.size <= 0:
-            continue
         
+        # detect aruco
+        detected_corners, ids = detect_aruco_in_image(frame, aruco_detector_parameters, aruco_dictionary)
+        if detected_corners.size <= 0: 
+            # draw empty detection
+            cv2.imshow('detection', frame)
+            if cv2.waitKey(10) == 27:
+                break
+            else:
+                continue
+        
+        # add points to calibration arrays
         object_points.append(aruco_points)
-        image_points.append(detected_corners)
+        # important. reshape detected corners from (1, 4, 2) to (4, 2)
+        image_points.append(detected_corners.reshape((4, 2)))
 
+        # draw detected aruco
+        cv2.aruco.drawDetectedMarkers(frame, detected_corners, ids, borderColor=(255, 0, 255))
+
+        # show frame 
         cv2.imshow('detection', frame)
         if cv2.waitKey(10) == 27:
             break
 
-    # object_points = np.array(object_points)
 
+    print('running calibrations...', end='', flush=True)
+    # run calibrations
     ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(
         objectPoints=object_points, 
         imagePoints=image_points, 
@@ -68,19 +99,26 @@ def main():
         cameraMatrix=None,
         distCoeffs=None
         )
+    
+    print('Done.')
+
+    # simple data structure for convenience
     calibration_data = {
         'camera_matrix': camera_matrix,
         'dist_coeffs': dist_coeffs,
         'rvecs': rvecs,
         'tvecs': tvecs
     }
-    with open(f'calibration_{source_id}.pkl', 'w'):
-        print(f'data for capture with id {source_id} saved in calibration{source_id}.pkl')
-        pickle.dump(calibration_data)
+
+    # save file
+    with open(f'calibration_{source_id}.pkl', 'wb') as out_file:
+        print(f'saving data for capture with id {source_id} in calibration{source_id}.pkl...', end='', flush=True)
+        pickle.dump(
+            obj=calibration_data,
+            file=out_file
+        )
+        print('Done!')
+
     
-
-
-    
-
 if __name__ == '__main__':
     main()
