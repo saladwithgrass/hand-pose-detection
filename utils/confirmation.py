9ceac7fbfef2ba956c1488c9547600bbe2608c5d
eskorobogatov <egod09080908@yandex.ru>
2025-01-16 16:56:15 +0300

    bug fix

--- utils/confirmation.py
+++ utils/confirmation.py
@@ -1,6 +1,6 @@
 def confirmation_prompt(text:str):
     answer = input(text + '[y/n]')
-    if answer.lower() in ['y, yes']:
+    if answer.lower() in ['y', 'yes']:
         return True
     elif answer.lower() in ['n', 'no']:
         return False
