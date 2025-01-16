f448f87e7898b2a327f74de203b0c7d4f6ddaa07
eskorobogatov <egod09080908@yandex.ru>
2025-01-16 17:35:57 +0300

    moved to another format of image size

--- calibration/calibration_data/add_image_size.py
+++ calibration/calibration_data/add_image_size.py
@@ -6,10 +6,11 @@ sys.path.append('../../')
 from utils.confirmation import confirmation_prompt
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('input_file', help='File which image size will be added to.')
-    parser.add_argument('width', help='Width that will be added to file.', type=int)
-    parser.add_argument('height', help='Height that will be added to file.', type=int)
+    parser.add_argument('-w', '--width', help='Width that will be added to file.', type=int, required=True)
+    parser.add_argument('-h', '--height', help='Height that will be added to file.', type=int, required=True)
+    parser.add_argument('-H', '--help', action='help', help='show this help message and exit')
     args = parser.parse_args()
     input_filename = args.input_file
     camera_dict = None
@@ -17,18 +18,21 @@ def main():
         camera_dict = pickle.load(input_file)
         
         # if calibration file already has it, ask for confirmation
-        if 'image_size' in camera_dict.keys():
+        if 'image_height' in camera_dict.keys() or 'image_width' in camera_dict.keys():
             # get confirmation from user
             to_write = None
             while to_write is None:
-                to_write = confirmation_prompt(f'File {input_filename} already has image_size. Are you sure you want to overwrite it?')
+                to_write = confirmation_prompt(f'File {input_filename} already has image size. Are you sure you want to overwrite it?')
             # return if user declined
             if not to_write:
+                print('Writing aborted.')
                 return
 
         # add field to dictionary
-        camera_dict['image_size'] = (args.height, args.width)
+        camera_dict['image_width'] = args.width
+        camera_dict['image_height'] = args.height
     
+    print('Writing image size.')
     # reopen file wor write
     with open(input_filename, 'wb') as output_file:
         pickle.dump(camera_dict, output_file)
--- positioning/triangulator.py
+++ positioning/triangulator.py
@@ -2,6 +2,10 @@ import numpy as np
 import cv2
 import pickle
 
+import sys
+sys.path.append('../')
+from utils.capture_opener import create_capture_from_json
+
 class CameraTriangulator():
 
     def __init__(self, 
@@ -9,34 +13,32 @@ class CameraTriangulator():
                     distortion_coefficients:list[np.ndarray],
                     cameras_rvecs:list[np.ndarray],
                     cameras_tvecs:list[np.ndarray],
-                    image_height:int,
-                    image_width:int
+                    image_width:int,
+                    image_height:int
                 ):
         # create self fields
         self.camera_matrices = camera_matrices
         self.dist_coeffs = distortion_coefficients
         self.rvecs = cameras_rvecs
         self.tvecs = cameras_tvecs
-        self.image_size = (image_height, image_width)
+        self.image_size = (image_width, image_height)
         self.new_cam_matrices = []
         self.undistortMaps = []
         self.projection_matrices = []
 
-        # create new optimal camera matrices
+        # create new optimal camera matrices and undistort maps
         for cam_matrix, dist_coeffs in zip(camera_matrices, distortion_coefficients):
             new_cam_matrix, roi = cv2.getOptimalNewCameraMatrix(
                 cameraMatrix=cam_matrix, 
                 distCoeffs=dist_coeffs, 
-                imageSize=self.image_size
+                imageSize=self.image_size,
+                alpha=0.5
                 )
             self.new_cam_matrices.append(new_cam_matrix)
-            print(cam_matrix)
-            print(new_cam_matrix)
-            print(' ')
             self.undistortMaps.append(cv2.initUndistortRectifyMap(
                 cameraMatrix=cam_matrix,
                 distCoeffs=dist_coeffs,
-                R=cv2.Mat(),
+                R=None,
                 newCameraMatrix=new_cam_matrix,
                 size=self.image_size,
                 m1type=cv2.CV_32FC1
@@ -48,8 +50,12 @@ class CameraTriangulator():
 
     def create_projection_matrix(self, rvec, tvec, cam_matrix):
         result = np.hstack((cv2.Rodrigues(rvec)[0], tvec))
-        reuslt = np.dot(cam_matrix, result)
+        result = np.dot(cam_matrix, result)
         return result
+
+    def undistort_image(self, image, camera_index):
+        map1, map2 = self.undistortMaps[camera_index]
+        return cv2.remap(image, map1=map1, map2=map2, interpolation=cv2.INTER_LINEAR)
 
     def triangulate(self, pixel_positions):
         pass
@@ -63,6 +69,7 @@ def create_triangulator_from_files(
     rvecs = []
     tvecs = []
     for intr_filename, orientation_filename in zip(intr_files, orientation_files):
+        image_size = None
         with open(intr_filename, 'rb') as intr_file, \
         open(orientation_filename, 'rb') as orientation_file:
             
@@ -71,11 +78,21 @@ def create_triangulator_from_files(
             cur_orientation = pickle.load(orientation_file)
             
             # read parameters
-            print(cur_intr)
             cur_cam_matrix = cur_intr['camera_matrix']
             cur_dist_coeffs = cur_intr['dist_coeffs']
+            cur_image_width = cur_intr['image_width']
+            cur_image_height = cur_intr['image_height']
+            cur_image_size = (cur_image_width, cur_image_height)
             cur_rvec = cur_orientation['rvec']
             cur_tvec = cur_orientation['tvec']
+
+            # check if image sizes are the same
+            if image_size is not None and image_size != cur_image_size:
+                print('ERROR: image sizes must be the same. Aborting.')
+                exit(1)
+            else:
+                image_size = cur_image_size
+                print(f'Image size is {image_size}')
 
             # save them to arrays
             cam_matrices.append(cur_cam_matrix)
@@ -88,7 +105,9 @@ def create_triangulator_from_files(
         camera_matrices=cam_matrices,
         distortion_coefficients=dist_coeffs,
         cameras_rvecs=rvecs,
-        cameras_tvecs=tvecs
+        cameras_tvecs=tvecs,
+        image_height=image_size[1],
+        image_width=image_size[0]
         )
             
 
@@ -99,4 +118,17 @@ if __name__ == '__main__':
     parser.add_argument('-or', '--orientation', nargs='+')
     args = parser.parse_args()
 
-    create_triangulator_from_files(args.intrinsics, args.orientation)
+    triangulator = create_triangulator_from_files(args.intrinsics, args.orientation)
+
+    cap = create_capture_from_json(2, '../config/capture_params.json')
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        undistorted = triangulator.undistort_image(frame, 0)
+        cv2.imshow('huh', undistorted)
+        if cv2.waitKey(10) == 27:
+            break
