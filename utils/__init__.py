2caafbc5f7fe9309421d58b1685b830326841c3f
jazzhands <egod09080908@yandex.ru>
2025-05-18 21:17:57 +0300

    added preload to connections

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,7 @@
 from utils import draw_utils
 print('Utils Module Imported')
-connection_dict = draw_utils.load_connections('config/hand_connections.json')
-colors_dict = draw_utils.load_colors('config/hand_colors.json', 'config/hand_connections.json')
+
+connections_dict, colors_dict = draw_utils.load_colors_and_connections(
+    'config/hand_colors.json', 
+    'config/hand_connections.json'
+)
